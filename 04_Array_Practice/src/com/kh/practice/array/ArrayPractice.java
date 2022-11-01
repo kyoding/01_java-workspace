ea7fd6069a208efbd6acc2cdcb3e7dfc725aac96
kyoding <minkyo0816@gmail.com>
2022-11-01 15:16:14 +0900

    11/01 !

--- 04_Array_Practice/src/com/kh/practice/array/ArrayPractice.java
+++ 04_Array_Practice/src/com/kh/practice/array/ArrayPractice.java
@@ -417,7 +417,7 @@ public class ArrayPractice {
 		} else if (yn1 == 'n' || yn1 == 'N') {
 			System.out.print("[");
 			for (int i = 0 ; i < arr.length ; i++) {
-				System.out.print(arr[i] + ",sss ");
+				System.out.print(arr[i] + ", ");
 			}
 			System.out.print("]");
 
